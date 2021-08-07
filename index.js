8fc7c69da7ee3fdcb304f64c18b03e40c0693240
DylanLaptop <dylannicolet@hotmail.com>
2021-08-07 18:29:39 +0200

    Add: JSON data

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const title = document.getElementById("title");
-
+const main = document.getElementById("main-content");
 
 title.textContent = "blablabla";
+main.setAttribute("style", "display:none");
+
+
+//work here
+
