ab592ef6ec5b3f3a1621e269c1b020aec002f230
DylanLaptop <dylannicolet@hotmail.com>
2021-08-14 15:17:49 +0200

    Add: profil-page filter

--- JavaScripts/profil-page.js
+++ JavaScripts/profil-page.js
@@ -7,7 +7,6 @@ const profilTagsSection = document.getElementById("profil-tags-id");
 const profilPicture = document.getElementById("profil-picture-id");
 
 
-
 ///access JSON database
 let apiRequest = new XMLHttpRequest();
 apiRequest.open('GET', '/database.json');
@@ -20,16 +19,19 @@ apiRequest.onreadystatechange = () => {
         const photographersData = data.photographers;
         let currentPhotographer = "";
 
+        //loop to detect current photographer page & data
         for (i=0; i<photographersData.length; i++){
             if (profilId.textContent == photographersData[i].id){
                 currentPhotographer = photographersData[i];
             }
         }
 
+        //add content to profil-banner
         profilName.textContent = currentPhotographer.name;
         profilCity.textContent = currentPhotographer.city+", " + currentPhotographer.country;
         profilTagline.textContent = currentPhotographer.tagline;
 
+        //loop  to create tags for profil-banner
         for (j=0; j<currentPhotographer.tags.length; j++){
             const newTagLink = document.createElement('a');
             newTagLink.textContent = "#" + currentPhotographer.tags[j];
@@ -37,7 +39,7 @@ apiRequest.onreadystatechange = () => {
             newTagLink.setAttribute("href", "#");
             profilTagsSection.appendChild(newTagLink);
         }
-
+        
         profilPicture.setAttribute("src", "/images/Photographers/" + currentPhotographer.portrait);
     }
 }
