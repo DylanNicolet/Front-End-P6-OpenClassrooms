8d241c242ab1af98bb3f2d29ef308ee90af0bb40
DylanLaptop <dylannicolet@hotmail.com>
2021-08-15 19:53:47 +0200

    Add: Functionality to Like buttons & total likes count

--- JavaScripts/profil-page.js
+++ JavaScripts/profil-page.js
@@ -6,6 +6,8 @@ const profilTagline = document.getElementById("profil-tagline-id");
 const profilTagsSection = document.getElementById("profil-tags-id");
 const profilPicture = document.getElementById("profil-picture-id");
 const mediaSection = document.getElementById("media-section-id");
+const totalLikesSection = document.getElementById("total-likes-id");
+const likeAndIcon = document.getElementById("like-and-icon");
 
 
 ///access JSON database
@@ -20,6 +22,7 @@ apiRequest.onreadystatechange = () => {
         const photographersData = data.photographers;
         const media = data.media;
         let currentPhotographer = "";
+        let totalNumberOfLikes = 0;
         
         //loop to detect current photographer page & data
         for (i=0; i<photographersData.length; i++){
@@ -50,29 +53,32 @@ apiRequest.onreadystatechange = () => {
             }
         })
 
-        //create media section
+        //loop to create media section
         for (x=0; x<currentPhotographerMedia.length; x++){
             let keys = Object.keys(currentPhotographerMedia[x]);
             
             const newMediaCard = document.createElement('section');
             const newMediaTitle = document.createElement('p');
-            const newMediaLikeCount = document.createElement('p');
+            let newMediaLikeCount = document.createElement('p');
             const newMediaLikeIcon = document.createElement('i');
             mediaSection.appendChild(newMediaCard);
             const newLikeSection = document.createElement('section');
 
             if(keys.includes("image")){
+                const newImgLink = document.createElement('a');
+                newImgLink.setAttribute("href", "#");
                 const newImg = document.createElement('img');
                 newImg.setAttribute("src", "/images/" + currentPhotographer.name + "/" + currentPhotographerMedia[x].image);
                 newImg.setAttribute("class", "media-section__media");
-                newMediaCard.appendChild(newImg);
+                newMediaCard.appendChild(newImgLink);
+                newImgLink.appendChild(newImg);
+
             }
 
             if(keys.includes("video")){
                 const newVideo = document.createElement('video');
                 newVideo.setAttribute("src", "/images/" + currentPhotographer.name + "/" + currentPhotographerMedia[x].video);
                 newVideo.setAttribute("type", "video/mp4");
-                newVideo.setAttribute("controls", "");
                 newVideo.setAttribute("class", "media-section__media");
                 newMediaCard.appendChild(newVideo);
             }
@@ -80,7 +86,9 @@ apiRequest.onreadystatechange = () => {
             newMediaTitle.textContent = currentPhotographerMedia[x].title;
             newMediaLikeCount.textContent = currentPhotographerMedia[x].likes;
             newMediaLikeIcon.setAttribute("class", "fas fa-heart");
+            newMediaLikeIcon.setAttribute("href", "#");
 
+            newMediaCard.setAttribute("class", "media-section__card");
             newMediaTitle.setAttribute("class", "media-section__title");
             newLikeSection.setAttribute("class", "media-section__likesection");
             newMediaLikeCount.setAttribute("class", "media-section__likecount");
@@ -89,10 +97,30 @@ apiRequest.onreadystatechange = () => {
             newMediaCard.appendChild(newLikeSection);
             newLikeSection.appendChild(newMediaLikeCount);
             newLikeSection.appendChild(newMediaLikeIcon);
-        }
+
+            //generate a total number of likes of all media items
+            totalNumberOfLikes += currentPhotographerMedia[x].likes;
+
+            //Add functionality to Like button for each media item
+            newMediaLikeIcon.addEventListener("click", () => {
+                totalNumberOfLikes++
+                newTotalLikes.textContent = totalNumberOfLikes;
+                newMediaLikeCount.textContent++
+            });
+        }  
+
+        //create total-likes section
+        const newTotalLikes = document.createElement('p');
+        const newLikesIcon = document.createElement('i');
+        const newPhotographersPrice = document.createElement('p');
 
         
-        
+        newLikesIcon.setAttribute("class", "fas fa-heart");
+        newPhotographersPrice.textContent = currentPhotographer.price + "$/day";
+        newTotalLikes.textContent = totalNumberOfLikes;
 
+        likeAndIcon.appendChild(newTotalLikes);
+        likeAndIcon.appendChild(newLikesIcon);
+        totalLikesSection.appendChild(newPhotographersPrice);
     }
 }
