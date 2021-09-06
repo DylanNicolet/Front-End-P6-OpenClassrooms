c687e1b0768da7eeea995099fd533537b84b1273
DylanLaptop <dylannicolet@hotmail.com>
2021-09-06 20:29:08 +0200

    Worked on: bug 1

--- JavaScripts/index.js
+++ JavaScripts/index.js
@@ -76,7 +76,7 @@ function createProfil(photographers){
         //add content from JSON file to new DOM elements
         newLink.setAttribute("href", "/profil-pages/" + photographers[z].name + ".html");
         newLink.setAttribute("aria-label", photographers[z].name);
-        newImg.setAttribute("src", "/images/Photographers/" + photographerImg);
+        newImg.setAttribute("src", "./images/Photographers/" + photographerImg);
         console.log("/images/Photographers/" + photographerImg);
         newImg.setAttribute("alt", "");
         newH2.textContent = photographers[z].name;
