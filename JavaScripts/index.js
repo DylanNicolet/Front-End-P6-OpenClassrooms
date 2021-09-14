10c5742197fd62c646440d1ca78a6986a909e943
DylanLaptop <dylannicolet@hotmail.com>
2021-09-14 13:09:21 +0200

    fix trial 1.3

--- JavaScripts/index.js
+++ JavaScripts/index.js
@@ -76,7 +76,8 @@ function createProfil(photographers){
         //add content from JSON file to new DOM elements
         newLink.setAttribute("href", "/profil-pages/" + photographers[z].name + ".html");
         newLink.setAttribute("aria-label", photographers[z].name);
-        newImg.setAttribute("src", "https://github.com/DylanNicolet/dylannicolet.github.io/tree/main/images/Photographers/" + photographerImg);
+        newImg.setAttribute("src", "./images/Photographers/" + photographerImg);
+        console.log("/images/Photographers/" + photographerImg);
         newImg.setAttribute("alt", "");
         newH2.textContent = photographers[z].name;
         newLocation.textContent = photographers[z].city + ", " + photographers[z].country;
